2ac9a424cfd39e7a132ba373dc6815d2bc1ee283
ElCholas <maurialdeco@gmail.com>
2022-08-01 23:11:42 -0300

    Incorporando Arrays

--- codigo.js
+++ codigo.js
@@ -6,55 +6,66 @@
 
 
 //Simulador Interactivo 
-let ropaDeportiva = prompt("Escriba el producto que desea adquirir y te dire su precio \n Camiseta Boca Juniors titular 2022 \n Camiseta River Plate titular 2022 \n Camiseta Argentina QATAR 2022 \n Short Boca Juniors titular 2022 \n Short River Plate titular 2022 \n Short Argentina QATAR 2022 \n Botines Nemeziz \n Botines Predator \n Botines X SpeedFlow \n Pelota QATAR 2022 \nPara salir escriba S");
-let precio = 0;
-
-function calculo(){
-    while (ropaDeportiva != "S") {
-        switch (ropaDeportiva) {
-            case "Camiseta Boca Juniors titular 2022":
-                console.log("Camiseta Boca Juniors $15.000")
-                precio = precio + 15000;
-                break;
-            case "Camiseta River Plate titular 2022":
-                console.log("Camiseta River Plate $16.500")
-                precio = precio + 16500;
-                break;
-            case "Camiseta Argentina QATAR 2022":
-                console.log("Camiseta Argentina $13.999")
-                precio = precio + 13999;
-                break;
-            case "Short Boca Juniors titular 2022":
-                console.log("Short Boca Juniors $7.000")
-                precio = precio + 7000;
-                break;
-            case "Short River Plate titular 2022":
-                console.log("Short River Plate $7.999")
-                precio = precio + 7999;
-                break;
-            case "Short Argentina QATAR 2022":
-                console.log("Short Argentina $6.500")
-                precio = precio + 6500;
-                break;
-                case "Botines Nemeziz":
-                    console.log("Nemeziz $16.000")
-                    precio = precio + 16000;
-                    break;
-            case "Botines Predator":
-                console.log("Predator $19.000")
-                precio = precio + 19000;
-                break;
-                case "Botines X SpeedFlow":
-                    console.log("SpeedFlow $22.000")
-                    precio = precio + 22000;
-                    break;
-            case "Pelota QATAR 2022":
-                console.log("Pelota QATAR 2022 $24.999")
-                precio = precio + 24999;
-                break;
-        }
-        ropaDeportiva = prompt("Escriba el producto que desea adquirir y te dire su precio \n Camiseta Boca Juniors titular 2022 \n Camiseta River Plate titular 2022 \n Camiseta Argentina QATAR 2022 \n Short Boca Juniors titular 2022 \n Short River Plate titular 2022 \n Short Argentina QATAR 2022 \n Botines Nemeziz \n Botines Predator \n Botines X SpeedFlow \n Pelota QATAR 2022 \nPara salir escriba S")
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
     }
-    console.log("Total a pagar: $" + precio);
-}
-calculo();
+    }
+    const productos = [];
+    let ropaDeportiva = prompt("Escriba el producto que desea adquirir y te dire su precio \n camiseta boca juniors titular 2022 \n camiseta river plate titular 2022 \n camiseta argentina qatar 2022 \n short boca juniors titular 2022 \n short river plate titular 2022 \n short argentina qatar 2022 \n botines nemeziz \n botines predator \n botines x speedflow \n pelota qatar 2022 \n(Para salir escriba s)".toLowerCase());
+    let precio = 0;
+    
+    function calculo(){
+    
+        while (ropaDeportiva != "s") {
+            switch (ropaDeportiva) {
+                case "camiseta boca juniors titular 2022":
+                    productos.push(new Producto("camiseta boca juniors titular 2022",15000));
+                    precio = precio + 15000;
+                    break;
+                case "camiseta river plate titular 2022":
+                    productos.push(new Producto("camiseta river plate titular 2022",16500));
+                    precio = precio + 16500;
+                    break;
+                case "camiseta argentina qatar 2022":
+                    productos.push(new Producto("camiseta argentina qatar 2022",13999));
+                    precio = precio + 13999;
+                    break;
+                case "short boca juniors titular 2022":
+                    productos.push(new Producto("short boca juniors titular 2022",7000));
+                    precio = precio + 7000;
+                    break;
+                case "short river plate titular 2022":
+                    productos.push(new Producto("short river plate titular 2022",7999));
+                    precio = precio + 7999;
+                    break;
+                case "short argentina qatar 2022":
+                    productos.push(new Producto("short argentina qatar 2022",
+                    6500));
+                    precio = precio + 6500;
+                    break;
+                    case "botines nemeziz":
+                        productos.push(new Producto("botines nemeziz",16000))
+                        precio = precio + 16000;
+                        break;
+                case "botines predator":
+                    productos.push(new Producto("botines predator",19000))
+                    precio = precio + 19000;
+                    break;
+                    case "botines x speedflow":
+                        productos.push(new Producto("botines x speedflow",22000))
+                        precio = precio + 22000;
+                        break;
+                case "pelota qatar 2022":
+                    productos.push(new Producto("pelota qatar 2022",24999))
+                    precio = precio + 24999;
+                    break;
+            }
+            console.log(productos)
+            ropaDeportiva = prompt("Escriba el producto que desea adquirir y te dire su precio \n camiseta boca juniors titular 2022 \n camiseta river plate titular 2022 \n camiseta argentina qatar 2022 \n short boca juniors titular 2022 \n short river plate titular 2022 \n short argentina qatar 2022 \n botines nemeziz \n botines predator \n botines x speedflow \n pelota qatar 2022 \npara salir escriba s")
+        }
+        console.log("Total a pagar: $" + precio);
+    }
+    calculo();
+    
