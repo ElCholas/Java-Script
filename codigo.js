d24cb5218d64cac1e36e92893d4fd04f9912d852
ElCholas <maurialdeco@gmail.com>
2022-08-06 17:41:06 -0300

    Aplicando Dom

--- codigo.js
+++ codigo.js
@@ -7,65 +7,62 @@ class Producto {
         this.original = original;
     }
 }
-const productos = [];
-/* Pide al usuario nombre de producto */
-let ropaDeportiva = prompt("Escriba el producto que desea adquirir y te dire su precio \n camiseta boca juniors titular 2022 \n camiseta river plate titular 2022 \n camiseta argentina qatar 2022 \n short boca juniors titular 2022 \n short river plate titular 2022 \n short argentina qatar 2022 \n botines nemeziz \n botines predator \n botines x speedflow \n pelota qatar 2022 \n(Para salir presione enter)".toLowerCase());
-let precio = 0;
-/* Suma+Productos */
-function calcular() {
+/* Array de productos */
+const productos = [
+    {
+        nombre:"Camiseta Boca Juniors", precio: 15000, img:"./img/boca.jpeg", alt:"Camiseta Boca Juniors",
+    },
 
-    while (ropaDeportiva != "") {
-        switch (ropaDeportiva) {
-            case "camiseta boca juniors titular 2022":
-                productos.push(new Producto("camiseta boca juniors titular 2022", 15000, true));
-                precio = precio + 15000;
-                break;
-            case "camiseta river plate titular 2022":
-                productos.push(new Producto("camiseta river plate titular 2022", 16500, true));
-                precio = precio + 16500;
-                break;
-            case "camiseta argentina qatar 2022":
-                productos.push(new Producto("camiseta argentina qatar 2022", 8000, false));
-                precio = precio + 13999;
-                break;
-            case "short boca juniors titular 2022":
-                productos.push(new Producto("short boca juniors titular 2022", 7000, true));
-                precio = precio + 7000;
-                break;
-            case "short river plate titular 2022":
-                productos.push(new Producto("short river plate titular 2022", 7999, true));
-                precio = precio + 7999;
-                break;
-            case "short argentina qatar 2022":
-                productos.push(new Producto("short argentina qatar 2022", 5000, false));
-                precio = precio + 6500;
-                break;
-            case "botines nemeziz":
-                productos.push(new Producto("botines nemeziz", 12000, false))
-                precio = precio + 17000;
-                break;
-            case "botines predator":
-                productos.push(new Producto("botines predator", 19000, true))
-                precio = precio + 19000;
-                break;
-            case "botines x speedflow":
-                productos.push(new Producto("botines x speedflow", 22000, true))
-                precio = precio + 22000;
-                break;
-            case "pelota qatar 2022":
-                productos.push(new Producto("pelota qatar 2022", 24999, true))
-                precio = precio + 24999;
-                break;
-        }
-        /* Muestra productos selecciondos */
-        console.table(productos)
-        ropaDeportiva = prompt("Escriba el producto que desea adquirir y te dire su precio \n camiseta boca juniors titular 2022 \n camiseta river plate titular 2022 \n camiseta argentina qatar 2022 \n short boca juniors titular 2022 \n short river plate titular 2022 \n short argentina qatar 2022 \n botines nemeziz \n botines predator \n botines x speedflow \n pelota qatar 2022 \npara salir presione enter")
-    }
-    /* Filtra producto */
-    const original = productos.filter((productos) => productos.original == false)
-    console.log(original)
-    console.log("Total a pagar: $" + precio);
+    {
+        nombre:"Camiseta River Plate", precio: 16500, img:"./img/river.jpeg", alt:"Camiseta River Plate",
+    },
+
+    {
+        nombre:"Camiseta Argentina Qatar", precio: 9999, img:"./img/Argentina.jpeg", alt:"Camiseta Argentina Qatar",
+    },
+
+    {
+        nombre:"Short Boca Juniors", precio: 7000, img:"./img/sBoca.jpg", alt:"Short Boca Juniors",
+    },
+
+    {
+        nombre:"Short River Plate", precio: 7999, img:"./img/sRiver.jpg", alt:"Short River Plate",
+    },
+
+    {
+        nombre:"Short Argentina Qatar", precio: 5500, img:"./img/sArgentina.jpg", alt:"Short Argetina Qatar",
+    },
+
+    {
+        nombre:"Botines Nemeziz", precio: 16000, img:"./img/bNemeziz.jpg", alt:"Botines Nemeziz",
+    },
+
+    {
+        nombre:"Botines Predator", precio: 19000, img:"./img/bPredator.jpg", alt:"Predator",
+    },
+
+    {
+        nombre:"Botines X SpeedFlow", precio: 22000, img:"./img/bXspeedflow.jpg", alt:"Botines X SpeedFlow",
+    },
+
+    {
+        nombre:"Pelota Qatar", precio: 24999, img:"./img/pQatar.jpg", alt:"Pelota Qatar",
+    },
+]
+
+/* Cards */
+let seccion = document.getElementById ("section")
+for (const producto of productos) {
+    let carta = document.createElement("div")
+    carta.innerHTML = `
+    <div class="card" style="width: 18rem;">
+    <img src="${producto.img}" class="card-img-top" alt="${producto.alt}">
+    <div class="card-body">
+        <h5 class="card-title">${producto.nombre}</h5>
+        <p class="card-text">${producto.precio}</p>
+        <button class="btn btn-primary btnPrincipal">Añadir</button>
+        </div>
+    </div>
+    `
+    seccion.append(carta)
 }
-/* Funcion Suma */
-calcular();
-
--- codigojs.js
+++
@@ -1,4 +0,0 @@
-//Complementario
-let numero= parseInt(prompt("Coloque un numero"))
-for(i=0; i<numero;i++){
-    console.log("Hola")}
