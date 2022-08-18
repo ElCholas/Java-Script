dda4b0f2df5d6af2d004315bd44cb3792a3056ee
ElCholas <maurialdeco@gmail.com>
2022-08-18 20:54:03 -0300

    Segunda entrega PF

--- codigo.js
+++ codigo.js
@@ -1,12 +1,22 @@
-// //Simulador Interactivo 
+//  Simulador Interactivo 
 /* Constructor */
 class Producto {
-    constructor(nombre, precio) {
+    constructor(nombre, precio, talle) {
         this.nombre = nombre;
         this.precio = parseFloat(precio);
+        this.talle = talle;
     }
 }
-/* Array de Productos */
+/* Boton eliminar */
+// const eliminarProducto = (eliminarProducto) => {
+//     const product = carrito.find((carrito) => carrito.id === eliminarProducto)
+//     const indice = carrito.indexOf(product)
+//     carrito.splice(indice, 1)
+//     agregarCarrito()
+// }
+/* Constantes */
+const precioTotal = document.getElementById("precioTotal")
+const carro = document.getElementById("contenedorCarrito")
 const carrito = []
 const productos = [
     {
@@ -14,64 +24,73 @@ const productos = [
         "nombre": "Camiseta Boca Juniors",
         "precio": 15000,
         "img": "./img/boca.jpeg",
+        "talle": "xl"
     },
-
+    
     {
         "id": 2,
         "nombre": "Camiseta River Plate",
         "precio": 16500,
         "img": "./img/river.jpeg",
+        "talle": "xl"
     },
-
+    
     {
         "id": 3,
         "nombre": "Camiseta Argentina Qatar",
         "precio": 9999,
         "img": "./img/Argentina.jpeg",
+        "talle": "xl"
     },
-
+    
     {
         "id": 4,
         "nombre": "Short Boca Juniors",
         "precio": 7000,
         "img": "./img/sBoca.jpg",
+        "talle": "l"
     },
-
+    
     {
         "id": 5,
         "nombre": "Short River Plate",
         "precio": 7999,
         "img": "./img/sRiver.jpg",
+        "talle": "l"
     },
-
+    
     {
         "id": 6,
         "nombre": "Short Argentina Qatar",
         "precio": 5500,
         "img": "./img/sArgentina.jpg",
+        "talle": "l"
     },
-
+    
     {
         "id": 7,
         "nombre": "Botines Nemeziz",
         "precio": 16000,
         "img": "./img/bNemeziz.jpg",
+        "talle": "40"
     },
-
+    
     {
         "id": 8,
         "nombre": "Botines Predator",
         "precio": 19000,
         "img": "./img/bPredator.jpg",
+        "talle": "40"
     },
-
+    
     {
         "id": 9,
         "nombre": "Botines X SpeedFlow",
         "precio": 22000,
         "img": "./img/bXspeedflow.jpg",
+        "talle": "40"
     },
-
+    
     {
         "id": 10,
         "nombre": "Pelota Qatar",
@@ -79,28 +98,45 @@ const productos = [
         "img": "./img/pQatar.jpg",
     }
 ]
-let precio = 0;
 /* Cards */
 const seccion = document.getElementById("section")
 for (const producto of productos) {
     let carta = document.createElement("div")
     carta.innerHTML = `
-        <div class="card" style="width: 18rem;">
+    <div class="card" style="width: 18rem;">
     <img src="${producto.img}" class="card-img-top">
     <div class="card-body">
     <h5 class="card-title">${producto.nombre}</h5>
-    <p class="card-text">${producto.precio}</p>
+    <p class="card-text">$${producto.precio}</p>
     <button class="btn btn-primary" id="boton${producto.id}">Añadir</button>
     </div>
-        </div>
+    </div>
     `
     seccion.append(carta)
     /* Evento Button */
-let button= document.getElementById(`boton${producto.id}`)
-button.addEventListener("click",(e)=>{
-    e.preventDefault()
-    console.log("agregaste el producto al carrito.")
-    console.log(producto)
-    carrito.push(producto)
-})
+    let button = document.getElementById(`boton${producto.id}`)
+    button.addEventListener("click", (e) => {
+        e.preventDefault()
+        console.log("agregaste el producto al carrito.")
+        console.log(producto)
+        carrito.push(producto)
+        localStorage.setItem("Carrito", JSON.stringify(carrito))
+        agregarCarrito()
+    })
+}
+/* Carrito */
+const agregarCarrito = () => {
+    carro.innerHTML = ""
+    carrito.forEach((carrito) => {
+        const div = document.createElement("div")
+        div.className = "carrito"
+        div.innerHTML = `
+        <img src="${carrito.img}" class="img"></img>
+        <p>${carrito.nombre}</p>
+        <p>$${carrito.precio}</p>
+        <button onclick= "eliminarProducto(${carrito.id})" class="Eliminar"><i class="fa-solid fa-trash-can logoTacho"></i></button>
+        `
+        carro.appendChild(div)
+    })
+    precioTotal.innerText = carrito.reduce((acumulador, productos) => acumulador + productos.precio, 0) 
 }
